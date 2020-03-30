d828e70c10f109e5366cdabc4de1683bef924d17
philipehuan <51161848+philipehuan@users.noreply.github.com>
2020-03-30 10:19:01 -0300

    Add files via upload

---
+++ Colabora/urls.py
@@ -0,0 +1,21 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from CoreApp.ViewSet import ColaboradorViewSet
+from CoreApp.ViewSet import DepartamentoViewSet
+from CoreApp.ViewSet import FormacaoViewSet
+from CoreApp.ViewSet import FuncaoViewSet
+
+
+router = DefaultRouter()
+router.register('colabora/coreapp/colaborador', ColaboradorViewSet)
+router.register('colabora/coreapp/departamento',DepartamentoViewSet)
+router.register('colabora/coreapp/formacao', FormacaoViewSet)
+router.register('colabora/coreapp/funcao', FuncaoViewSet)
+
+
+urlpatterns = [
+    path('',include(router.urls)),
+    path('admin/', admin.site.urls),
+]
