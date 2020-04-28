1efc0725b0af97fc60ac1a6f6aa6cdca2e2ff2fc
Philipe Huan Ramos Lima <philipehuan@live.com>
2020-04-28 17:31:33 -0300

    Update urls.py

--- Colabora/urls.py
+++ Colabora/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from CoreApp.ViewSet import ColaboradorViewSet, Colaborador_get_info
+from CoreApp.ViewSet import ColaboradorViewSet
 from CoreApp.ViewSet import DepartamentoViewSet
 from CoreApp.ViewSet import FormacaoViewSet
 from CoreApp.ViewSet import FuncaoViewSet
